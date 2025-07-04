31de1fedd770cd82240ba4daadc13ba7c85fc543
anishumar <anishumar786@gmail.com>
2025-07-04 15:44:32 +0530

    Initial Commit

---
+++ Creatist/CreatistApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatistApp.swift
+//  Creatist
+//
+//  Created by Anish Umar on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CreatistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
